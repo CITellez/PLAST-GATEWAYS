a0ea28e9f012c21fd51c0fcf416e5be4ebf8eb64
CITellez <40807244+CITellez@users.noreply.github.com>
2018-07-16 20:57:57 -0500

    It's more clear to read

---
+++ FIND_SHEETS_TXT_V4.py
@@ -0,0 +1,141 @@
+from openpyxl import *
+import time
+import sys  
+
+#reload(sys)  
+#sys.setdefaultencoding('utf8')
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+#////////////////////// Open files, excel, databases and .py   ////////////////////////////////////
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+try:
+    FILE_PATH = 'D:\Python\E8A34000.xlsx'
+    wb = load_workbook(FILE_PATH, read_only=True)
+except:
+    print 'El excel donde estan los requerimientos no esta el la direccion especificada'
+    
+try:
+    file_CANC = open('D:\Python\PDT_E3A_R2_CCAN_Hand_Edited_Version2.dbc', 'r')
+except:
+    print 'La dbc de can C no se encuentra en la direccion especifica o el nombre no es correcto'
+    
+try:
+    file_CANB = open('D:\Python\PDT20_E1A_R1_BHCAN.dbc', 'r')
+except:
+    print 'La dbc de can B no se encuentra en la direccion especifica o el nombre no es correcto'
+
+try:
+    results = open('D:\Python\results.txt','w')
+    results.write('hello')
+except:
+    print 'El documento de resultados no puedo ser creado'
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+#//////////////////////// Initial code (it doesn't change)/////////////////////////////////////////
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+print '# Autor: Carlos Ichiro Tellez Urakami'
+print '# Purpose: Gateways automatization'
+print '# Date:'
+print '#/////////////////////////////////////'
+print 'from PlatformFiles import *'
+print 'from _UserFunctions import *'
+print 'from openpyxl import *'
+print 'import time'
+
+print 'def Gateways(testcase):'
+print '    testcase.safety_requirement = ,'  #Necesitas ver como poner un caracter '' dentro
+print '    testcase.traceability = ,'             
+print '    can_instance = CANoe()'
+print '    try:'
+print '        can_instance.Start()'
+print '    except:'
+print '        print("La simulacion ya esta corriendo")'
+
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+#////////////////////////      Variables to be used      //////////////////////////////////////////
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+document_canc = []
+document_canb = []
+tatal = 0
+desde = 0
+hasta = 0
+Chanel = {'C':1, 'B':2, 1:3, 2:4, 3:5}
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+#/////////////////////Extracting the importan information from the DBCs ///////////////////////////
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+
+for line in file_CANC:
+    if line.startswith(' SG_'):
+        document_canc.append(line)
+size = len(document_canc) 
+print size
+
+for line in file_CANB:
+    if line.startswith(' SG_'):
+        document_canb.append(line)
+size = len(document_canb) 
+print size
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+#/////////////////////     Start working with the excel information     ///////////////////////////
+#//////////////////////////////////////////////////////////////////////////////////////////////////          
+for sheet in wb:
+    print(sheet.title)    
+    time.sleep(.1)
+
+    for num in range(3,18):
+        sender_chanel       = Chanel[sheet.cell(row = num, column = 1).value]
+        receiver_chanel     = Chanel[sheet.cell(row = num, column = 2).value]
+        sender_msg          = sheet.cell(row = num, column = 4).value
+        sen_sig             = sheet.cell(row = num, column = 5).value
+        sen_sig             = sen_sig.encode("utf-8")
+        receiver_msg        = sheet.cell(row = num, column = 7).value
+        receiver_sig        = sheet.cell(row = num, column = 8).value
+        requirement         = sheet.cell(row = num, column = 12).value
+
+        print 'from chanel' , sender_chanel, 'to chanel', receiver_chanel
+        print sender_msg,'::',sen_sig,'--- to ---',receiver_msg,'::',receiver_sig
+        time.sleep(1)
+        #identify the databese to be used 
+        for signal_line in document_canc:
+            if signal_line.find(sen_sig) != -1:
+                print signal_line
+                signal = signal_line #what is returning 
+                sizee = len(signal) 
+                print sizee
+                indexDot = signal.find("@")
+                
+                desde = signal[indexDot+1]
+                hasta = int(signal[indexDot-1])
+        total = (2**hasta)
+        print total
+
+        print 'for send in range(0,',total,'):'                    
+        print '    can_instance.SetSignalValue(', sender_chanel , ',' , sender_msg, ',' ,sen_sig, ',' ,'send' ,')'
+        time.sleep(.1)
+        print '    can_instance.SetSignalValue(', sender_chanel , ',' , sender_msg, ',' ,sen_sig, ',' ,'send' ,')'
+        time.sleep(.1)
+        print '    can_instance.SetSignalValue(', sender_chanel , ',' , sender_msg, ',' ,sen_sig, ',' ,'send' ,')'
+        time.sleep(.1)
+        print '    can_instance.SetSignalValue(', sender_chanel , ',' , sender_msg, ',' ,sen_sig, ',' ,'send' ,')'
+        time.sleep(.1)
+        print '    can_instance.SetSignalValue(', sender_chanel , ',' , sender_msg, ',' ,sen_sig, ',' ,'send' ,')'
+        time.sleep(.1)
+
+        print '    response_receiver = can_instance.GetSignalValue(',receiver_chanel,',',receiver_msg,',',receiver_sig,')'
+        time.sleep(.1)
+        print '    response_receiver = can_instance.GetSignalValue(',receiver_chanel,',',receiver_msg,',',receiver_sig,')'
+        time.sleep(.1)
+        print '    response_receiver = can_instance.GetSignalValue(',receiver_chanel,',',receiver_msg,',',receiver_sig,')'
+        time.sleep(.1)
+        print '    response_receiver = can_instance.GetSignalValue(',receiver_chanel,',',receiver_msg,',',receiver_sig,')'
+        time.sleep(.1)       
+    '''
+        try:
+            assert(response_receiver == response_sender)
+        except
+            print 'El GATEWAY',sender_msg,'::',sen_sig,'--- to ---',receiver_msg,'::',receiver_sig,'NO FUNCIONA'
+            
+    '''
